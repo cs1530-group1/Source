83a37b907441ab3069fdf31d98cb0c02cd6a1bfb
mra48 <mra48@pitt.edu>
2015-06-13 19:38:51 -0400

    Finished addPlants, initialize

--- garden.java
+++ garden.java
@@ -7,9 +7,10 @@ import java.io.*;
 
 public class garden {
 
-    //in add plant, still need to modify old file
-    //work on initialize plants
-
+    //Add plants is done
+    //initialize species is done
+    //initialize plants is done
+    //work on add species
     
     public static void main(String[] args)
     {   
@@ -18,7 +19,7 @@ public class garden {
         ArrayList<species> speciesList = initializeSpecies();
         ArrayList<plant> plantList = new ArrayList<>(200);
         
-        initializePlants(); //get all possible plants from the garden
+        initializePlants(plantList); //get all possible plants from the garden
         
         success = addPlant(plantList, speciesList);
         
@@ -70,18 +71,59 @@ public class garden {
     }    
     
     //initialize plants from plantdata.txt
-    public static boolean initializePlants()
+    public static boolean initializePlants(ArrayList<plant> plantList)
     {
+        String  plantData = "plantdata.txt";
+        BufferedReader infile;
+        
+        //try to read in data from plantdata.txt
+        try
+        {
+            infile = new BufferedReader(new FileReader(plantData));
+            int numPlants = Integer.parseInt(infile.readLine());
+            
+            //initialize the list of species by parsing the input file speciesdata.txt
+            for (int i = 0; i < numPlants; i++)
+            {
+                String plantDescription = infile.readLine();
+                String[] parsed = plantDescription.split(":");
+                int x = Integer.parseInt(parsed[0]);
+                int y = Integer.parseInt(parsed[1]);
+                String type = parsed[2];
+                String sun = parsed[3];
+                int prune = Integer.parseInt(parsed[4]);
+                int water = Integer.parseInt(parsed[5]);
+                int fertilize = Integer.parseInt(parsed[6]);
+                
+                species newSpecies = new species(type, sun, prune, water, fertilize);
+                plant newPlant = new plant(x, y, newSpecies);
+                
+                plantList.add(newPlant);
+            }
+            
+            infile.close();
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("Input File was not found. Please try again");
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Could not read plantdata.txt. Please try again");
+        }
+        
         return true;
     }
     
-    //add plant to garden
+    //add plant to garden and to plantdata.txt
     public static boolean addPlant(ArrayList<plant> plantList, ArrayList<species> speciesList)
     {
         Scanner reader = new Scanner(System.in);
         String species;
         int x, y;
         
+        String plantData = "plantdata.txt";
+                
         //this simulates the user interface for the app
         System.out.println("Enter a species name");
         species = reader.next();
@@ -93,6 +135,7 @@ public class garden {
         reader.close();
         
         boolean found = false;
+        
         for(species s: speciesList)
         {
             if (s.type.equalsIgnoreCase(species))
@@ -102,19 +145,57 @@ public class garden {
                 int prune = s.prune;
                 String type = s.type;
                 int fertilize = s.fertilize;
+                int newNum;
                 
                 //update plantdata.txt with new plant
                 try
                 {
-                    PrintWriter out = new PrintWriter("plantdata.txt");
-                    out.print(Integer.toString(x) + Integer.toString(y) + ":");
-                    out.print(type + ":" + sun + ":");
-                    out.print(Integer.toString(prune) + ":"  + Integer.toString(water) + ":" + Integer.toString(fertilize) + "\n");
+                    
+                    BufferedReader infile = new BufferedReader(new FileReader(plantData));
+                    int numPlants = Integer.parseInt(infile.readLine());         
+                    newNum = numPlants + 1;
+                    PrintWriter out = new PrintWriter("tempFile.txt");
+                    out.write(Integer.toString(newNum) + "\r\n");
                     out.flush();
+                    
+                    //make sure there are plants in plantdata.txt file before trying to read more lines
+                    if (numPlants > 0)
+                    {
+                        for (int i = 0; i < (numPlants); i++)
+                        {
+                            //copy plantdata.txt file to tempFile.txt
+                            out.write(infile.readLine() + "\r\n");
+                        }                            
+                    }
+                
+
+                    //print new plant data to plantdata.txt
+                    out.print(Integer.toString(x) + ":" + Integer.toString(y) + ":");
+                    out.print(type + ":" + sun + ":");
+                    out.print(Integer.toString(prune) + ":"  + Integer.toString(water) + ":" + Integer.toString(fertilize) + "\r\n");
+                    out.flush();
+                    
+                    out.close();
+                    infile.close();
+                    
+                    File tempFile = new File("tempFile.txt");                                                            
+                    File plantDataFile = new File("plantdata.txt");
+                    
+                    //delete plantdata.txt file (warning: this might not work on android)
+                    plantDataFile.delete();
+                    
+                    //rename tempfile to plantdata.txt (warning: this might not work on android)
+                    tempFile.renameTo(plantDataFile);
+                    
+
                 }
                 catch (FileNotFoundException ex)
                 {
                     System.out.println("The file plantdata.txt was not found. Please try again");
+                }
+                catch (IOException ex)
+                {
+                    System.out.println("Cant read from plantdata.txt. Please try again");
                 }
 
                 species newSpecies = new species(type, sun, prune, water, fertilize);
